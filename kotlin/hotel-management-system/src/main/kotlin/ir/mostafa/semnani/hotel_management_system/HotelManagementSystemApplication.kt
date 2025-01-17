56737d03ca19aa956468548e66483c4ea947d6a7
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-01-17 12:53:19 +0330

    init kotlin project

---
+++ kotlin/hotel-management-system/src/main/kotlin/ir/mostafa/semnani/hotel_management_system/HotelManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package ir.mostafa.semnani.hotel_management_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelManagementSystemApplication>(*args)
+}
---
+++ kotlin/hotel-management-system/src/test/kotlin/ir/mostafa/semnani/hotel_management_system/HotelManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.hotel_management_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
