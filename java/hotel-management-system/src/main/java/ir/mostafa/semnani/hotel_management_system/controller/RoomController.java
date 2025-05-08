1013f6f98d48cc428141da2c72e6834d094f026d
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-05-08 19:01:29 +0330

    init java reactive project

--- java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/controller/RoomController.java
+++ java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/controller/RoomController.java
@@ -1,13 +1,10 @@
 package ir.mostafa.semnani.hotel_management_system.controller;
 
-import ir.mostafa.semnani.hotel_management_system.entity.Room;
 import ir.mostafa.semnani.hotel_management_system.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
@@ -15,10 +12,5 @@ import java.util.List;
 public class RoomController {
 
     private final RoomService roomService;
-
-    @GetMapping
-    public List<Room> getAllRooms() {
-        return roomService.findAll();
-    }
 
 }
--- java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/entity/Room.java
+++ java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/entity/Room.java
@@ -1,16 +1,9 @@
 package ir.mostafa.semnani.hotel_management_system.entity;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "room")
 public class Room {
 
-    @Id
-    @GeneratedValue
     private Long id;
 
-    @Column(name = "number_of_beds")
     private int numberOfBeds;
 
 }
