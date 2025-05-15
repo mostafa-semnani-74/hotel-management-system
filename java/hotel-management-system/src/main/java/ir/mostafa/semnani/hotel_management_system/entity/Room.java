7c0728f58e301e5e153a3e9bb6d09ef7f5d252d4
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-05-15 16:54:03 +0330

    add r2dbc postgres config

--- java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/entity/Room.java
+++ java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/entity/Room.java
@@ -1,9 +1,20 @@
 package ir.mostafa.semnani.hotel_management_system.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("room")
+@Getter
+@Setter
 public class Room {
 
+    @Id
     private Long id;
 
+    @Column("number_of_beds")
     private int numberOfBeds;
 
 }
