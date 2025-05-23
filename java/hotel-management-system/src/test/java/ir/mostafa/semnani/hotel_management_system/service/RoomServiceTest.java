b9d1c746036575b4a9b06e5182f2538a061b933b
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-05-23 12:09:09 +0330

    add postgres test container

--- java/hotel-management-system/src/test/java/ir/mostafa/semnani/hotel_management_system/service/RoomServiceTest.java
+++ java/hotel-management-system/src/test/java/ir/mostafa/semnani/hotel_management_system/service/RoomServiceTest.java
@@ -1,17 +1,45 @@
 package ir.mostafa.semnani.hotel_management_system.service;
 
 import ir.mostafa.semnani.hotel_management_system.dto.request.SaveRoomRequestDTO;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
+//@Testcontainers
+@ActiveProfiles("test")
 public class RoomServiceTest {
 
     @Autowired
     private RoomService roomService;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     public void save_success_test() {
---
+++ java/hotel-management-system/src/test/resources/schema.sql
@@ -0,0 +1,5 @@
+create table room
+(
+    id             serial primary key,
+    number_of_beds integer
+);
