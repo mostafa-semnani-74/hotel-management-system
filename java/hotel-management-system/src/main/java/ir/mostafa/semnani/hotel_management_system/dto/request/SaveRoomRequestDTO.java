a0af84dd35fb8126a257a2c2bea9c578d9ca8987
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-06-04 11:37:08 +0330

    add validation for save room dto

--- java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/dto/request/SaveRoomRequestDTO.java
+++ java/hotel-management-system/src/main/java/ir/mostafa/semnani/hotel_management_system/dto/request/SaveRoomRequestDTO.java
@@ -1,6 +1,13 @@
 package ir.mostafa.semnani.hotel_management_system.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record SaveRoomRequestDTO(
+        @NotNull(message = "numberOfBeds should not be null")
+        @Min(value = 1, message = "numberOfBeds should be greater than 0")
+        @Max(value = 4, message = "numberOfBeds should be less than 5")
         int numberOfBeds
 ) {
 }
