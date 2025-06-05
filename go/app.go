081acab59fa23fda3c8c044701a03bf042e7beb0
mostafa semnani <mostafa.semnani.74@gmail.com>
2025-06-05 09:03:04 +0330

    init get all rooms prototype with gin framework

--- go/app.go
+++ go/app.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Room struct {
+	ID           uint64 `json:"id"`
+	NumberOfBeds uint32 `json:"numberOfBeds"`
+}
+
+var rooms = []Room{
+	{ID: 1, NumberOfBeds: 3},
+}
+
+func getRooms(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, rooms)
+}
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+
+	router.GET("/api/v1/rooms", getRooms)
+
+	router.Run("localhost:8080")
 }
