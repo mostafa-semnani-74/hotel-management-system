fbdbb6bb84e31b4f655dba1ba5e380a3232d10a6
mostafa semnani <mostafa.semnani.74@gmail.com>
2025-06-12 15:14:32 +0330

    add get all impl with gorm

--- go/model/room.go
+++ go/model/room.go
@@ -1,6 +1,6 @@
 package model
 
 type Room struct {
-	ID           uint64 `json:"id"`
-	NumberOfBeds uint32 `json:"numberOfBeds"`
+	ID           uint64 `gorm:"type:int;primaryKey" json:"id"`
+	NumberOfBeds uint32 `gorm:"type:int" json:"numberOfBeds"`
 }
--- go/service/room_service.go
+++ go/service/room_service.go
@@ -1,16 +1,30 @@
 package service
 
 import (
+	"fmt"
 	"hms/model"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var rooms = []model.Room{
-	{ID: 1, NumberOfBeds: 3},
-}
-
 func GetRooms(c *gin.Context) {
+	dsn := "host=localhost user=postgres password=1234 dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var rooms []model.Room
+	result := db.Find(&rooms) // finds all users
+
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	fmt.Println("get all rooms : ", rooms)
+
 	c.IndentedJSON(http.StatusOK, rooms)
 }
