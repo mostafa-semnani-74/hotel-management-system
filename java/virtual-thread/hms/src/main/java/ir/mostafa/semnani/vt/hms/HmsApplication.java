e04f2e2002465cb65a9d74cdd6c9b1f91c1a70d6
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2025-07-04 10:10:18 +0330

    init project

---
+++ java/virtual-thread/hms/src/main/java/ir/mostafa/semnani/vt/hms/HmsApplication.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.vt.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApplication.class, args);
+	}
+
+}
---
+++ java/virtual-thread/hms/src/test/java/ir/mostafa/semnani/vt/hms/HmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.vt.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
